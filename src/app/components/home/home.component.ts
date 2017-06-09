c03617d7660f73deb2757f6a605d636605066706
sanket238 <sanketpatel238@gmail.com>
2017-06-09 14:25:36 +0530

    changed the home ui

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFire} from 'angularfire2';
 import {FlashMessagesService} from 'angular2-flash-messages';
+import {FirebaseService} from '../../services/firebase.service';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +10,22 @@ import {FlashMessagesService} from 'angular2-flash-messages';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  listings:any;
+  id:any;
   constructor(
+     private firebaseService: FirebaseService,
+    private router:Router,
+    private route:ActivatedRoute,
     public af:AngularFire,
     public flashMessage:FlashMessagesService
   ) { }
 
   ngOnInit() {
+    this.firebaseService.getListings().subscribe(listings => {
+      console.log(listings);
+      this.listings = listings;
+
+    });
   }
 
   login(){
